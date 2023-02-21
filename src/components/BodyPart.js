acd3cce126a16c2cdd53017aacd89e6ec0588d40
IdrisGuloyev <idrisguloyev@gmail.com>
2023-02-21 20:57:52 +0600

    AddedGIfUrlOfExercises

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -19,8 +19,15 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => {
             cursor: 'pointer', 
             gap: '47px'
         }}  
+        onClick={() => {
+          setBodyPart(item);
+          window.scrollTo({top: 1800, left: 100, behavior: 'smooth'})
+        }}
     >
-        <img src={Icon} alt='dumbbell' styles={{ width: '40px', height: '40px'}}/>
+        <img src={Icon} alt='dumbbell' style={{ width: '40px', height: '40px'}}/>
+        <Typography fontSize='24px' fontWeight='bold' color='#3A1212' textTransform='capitalize'>
+          {item}
+        </Typography>
     </Stack>
   )
 }
