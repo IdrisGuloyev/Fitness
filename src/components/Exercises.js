d42d00b0a04fe7137a459a739b2abd532c55af92
IdrisGuloyev <idrisguloyev@gmail.com>
2023-02-22 20:34:11 +0600

    UpdateExercises.js

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -20,6 +20,22 @@ const Exercises = ({ exercises, setExercises, bodyPart}) => {
     window.scrollTo({ top: '1800px', behaviorL: 'smooth' })
   }
 
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      let exercisesData = [];
+
+      if(bodyPart === 'all') {
+        exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+      } else {
+        exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, exerciseOptions);
+      }
+
+      setExercises(exercisesData);
+    }
+
+    fetchExercisesData();
+  }, [bodyPart]);
+
   return (
    <Box id='exercises'
         sx={{mt: { lg: '110px' }}}
